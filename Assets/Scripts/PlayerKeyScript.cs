3af5c1979d0614309c675912fdadf0746119733d
zuzuba23 <tavi2303@yahoo.com>
2017-04-13 12:15:41 +0300

    Am pus aplicatia

---
+++ Assets/Scripts/PlayerKeyScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKeyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetMouseButtonDown(0)){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)){
+				if (hit.transform.tag == "door") {
+					float distance = Vector3.Distance (transform.position, hit.transform.position);
+					if (distance <= 3) {
+						foreach (GameObject g in GameObject.FindGameObjectsWithTag("door"))
+							Destroy (g);
+						transform.position = new Vector3 (0, 1, 50);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/WebSocketManagerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class WebSocketManagerScript : MonoBehaviour {
+
+	public WebSocket ws;
+	// Use this for initialization
+	IEnumerator Start () {
+		ws = new WebSocket (new System.Uri("wss://timf.upg-ploiesti.ro:443/3d/viznet/ws/w/2"));
+		yield return StartCoroutine(ws.Connect ());
+		ws.SendString ("time");
+		yield return new WaitForSeconds (1);
+		Debug.Log (ws.RecvString ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
